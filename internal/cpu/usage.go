44adae54ef4df3bbdc82efd5dc3703496b484a52
nerfthisdev <353054@niuitmo.ru>
2025-05-05 00:55:16 +0700

    feat: rewrite usage.go with better structure

--- internal/cpu/usage.go
+++ internal/cpu/usage.go
@@ -7,49 +7,91 @@ import (
 	"time"
 )
 
-func readCPUStat() (uint64, uint64, error) {
-	data, err := os.ReadFile("/proc/stat")
-	if err != nil {
-		return 0, 0, err
-	}
-
-	fields := strings.Fields(strings.Split(string(data), "\n")[0])[1:]
-
-	var values []uint64
-
-	for _, f := range fields {
-		v, _ := strconv.ParseUint(f, 10, 64)
-		values = append(values, v)
-	}
-
-	idle := values[3]
-	var total uint64
-
-	for _, v := range values {
-		total += v
-	}
-
-	return idle, total, nil
+type CPUStats struct {
+	TotalUsage float64
+	CoresUsage map[string]float64
+	BTime      uint64
 }
 
-func GetCPUUsage() (float64, error) {
-	idle1, total1, err := readCPUStat()
+func sumSlice(slice []uint64) uint64 {
+	var sum uint64
+	for _, v := range slice {
+		sum += v
+	}
+	return sum
+}
 
+func readStatSnapshot() (map[string][]uint64, uint64, error) {
+	data, err := os.ReadFile("/proc/stat")
 	if err != nil {
-		return 0, err
+		return nil, 0, err
 	}
 
-	time.Sleep(1 * time.Second)
-	idle2, total2, err := readCPUStat()
+	result := make(map[string][]uint64)
+	var btime uint64
+
+	lines := strings.Split(string(data), "\n")
+	for _, line := range lines {
+		fields := strings.Fields(line)
+		if len(fields) == 0 {
+			continue
+		}
+
+		key := fields[0]
+		if key == "btime" {
+			btime, _ = strconv.ParseUint(fields[1], 10, 64)
+			continue
+		}
+
+		if key == "cpu" || strings.HasPrefix(key, "cpu") {
+			var nums []uint64
+			for _, f := range fields[1:] {
+				n, err := strconv.ParseUint(f, 10, 64)
+				if err != nil {
+					return nil, 0, err
+				}
+				nums = append(nums, n)
+			}
+			result[key] = nums
+		}
+	}
+	return result, btime, nil
+}
+
+func GetCpuStats() (CPUStats, error) {
+	s1, btime, err := readStatSnapshot()
 
 	if err != nil {
-		return 0, err
+		return CPUStats{}, err
 	}
 
-	deltaIdle := idle2 - idle1
-	deltaTotal := total2 - total1
+	time.Sleep(100 * time.Millisecond)
 
-	cpuUsage := (1.0 - float64(deltaIdle)/float64(deltaTotal)) * 100
+	s2, _, err := readStatSnapshot()
 
-	return cpuUsage, nil
+	if err != nil {
+		return CPUStats{}, err
+	}
+
+	stats := CPUStats{
+		CoresUsage: make(map[string]float64),
+		BTime:      btime,
+	}
+
+	for cpu := range s1 {
+		idle1 := s1[cpu][3]
+		idle2 := s2[cpu][3]
+		total1 := sumSlice(s1[cpu])
+		total2 := sumSlice(s2[cpu])
+		usage := 1.0 - float64(idle2-idle1)/float64(total2-total1)
+		usage *= 100.0
+
+		if cpu == "cpu" {
+			stats.TotalUsage = usage
+		} else {
+			stats.CoresUsage[cpu] = usage
+		}
+	}
+
+	return stats, nil
 }
