8beb27009eecc68b01573413efae67df9e95b95d
nerfthisdev <353054@niuitmo.ru>
2025-05-05 01:47:29 +0700

    feat: add Stream stats realisation and main func with server starter

---
+++ cmd/server/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"log"
+	"net"
+	"time"
+
+	"github.com/nerfthisdev/kronos-server/internal/cpu"
+	pb "github.com/nerfthisdev/kronos-server/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+type monitorServer struct {
+	pb.UnimplementedMonitorServiceServer
+}
+
+func (s *monitorServer) StreamStats(_ *pb.StatsRequest, stream grpc.ServerStreamingServer[pb.StatsResponse]) error {
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			stats, err := cpu.GetCpuStats()
+
+			if err != nil {
+				return err
+			}
+			var cores []*pb.CPUCoreUsage
+			for core, usage := range stats.CoresUsage {
+				cores = append(cores, &pb.CPUCoreUsage{
+					Core:  core,
+					Usage: usage,
+				})
+			}
+			cpuResp := &pb.CPUStatsResponse{
+				TotalUsage: stats.TotalUsage,
+				Btime:      stats.BTime,
+				CoresUsage: cores,
+			}
+
+			resp := &pb.StatsResponse{
+				CpuUsageTotal: cpuResp,
+				MemoryUsage:   0,
+				DiskUsage:     0,
+			}
+			if err := stream.Send(resp); err != nil {
+				return err
+			}
+		}
+	}
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterMonitorServiceServer(grpcServer, &monitorServer{})
+
+	log.Println("gRPC server listening on :50051")
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
