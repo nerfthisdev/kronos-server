c8d5d62ad0368be1f6de680a2b1160e81ed53892
nerfthisdev <353054@niuitmo.ru>
2025-05-05 01:55:47 +0700

    feat: add simple client

---
+++ cmd/client/main.go
@@ -0,0 +1,43 @@
+package client
+
+import (
+	"context"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	monitorpb "github.com/nerfthisdev/kronos-server/proto"
+	pb "github.com/nerfthisdev/kronos-server/proto"
+)
+
+func main() {
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewMonitorServiceClient(conn)
+	stream, err := client.StreamStats(context.Background(), &monitorpb.StatsRequest{})
+	if err != nil {
+		log.Fatalf("Error on StreamStats: %v", err)
+	}
+
+	for {
+		resp, err := stream.Recv()
+		if err != nil {
+			log.Fatalf("Error receiving: %v", err)
+		}
+
+		log.Printf("%f", resp.DiskUsage)
+		log.Printf("%f", resp.MemoryUsage)
+
+		cpuStats := resp.CpuUsageTotal
+
+		log.Printf("Total: %.2f%%, BTime: %d", cpuStats.TotalUsage, cpuStats.Btime)
+		for _, core := range cpuStats.CoresUsage {
+			log.Printf("Core %s: %.2f%%", core.Core, core.Usage)
+		}
+	}
+}
