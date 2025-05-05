c21a30c182f9a285d1b1e4734bbd8c50757ef0c0
nerfthisdev <353054@niuitmo.ru>
2025-05-05 14:26:53 +0700

    feat: update app component

--- cmd/frontend/src/App.tsx
+++ cmd/frontend/src/App.tsx
@@ -1,7 +1,13 @@
+import Dashboard from "./components/Dashboard";
+
+
 const App = () => {
   return (
-    <div></div>
-  )
+    <div style={{ padding: '2rem' }}>
+      <h1>System Monitor Dashboard</h1>
+      <Dashboard />
+    </div>
+  );
 }
 
 export default App
